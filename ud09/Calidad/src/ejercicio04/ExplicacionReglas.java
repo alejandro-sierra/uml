988cd69bb9dc2d66a60cbc90b41c8d9c647de27c
Alejandro Sierra <alejandrosierraballesta@gmail.com>
2021-05-04 16:25:49 +0200

    ejercicio04

---
+++ ud09/Calidad/src/ejercicio04/ExplicacionReglas.java
@@ -0,0 +1,21 @@
+package ejercicio04;
+
+public class ExplicacionReglas {
+	/*
+	 *Best Practices
+	 *1.ForLoopVariableCount
+	 *	
+	 *2.
+	 *3.
+	 *
+	 *Code Style
+	 *1.
+	 *2.
+	 *3.
+	 *
+	 *Design
+	 *1.
+	 *2.
+	 *3.
+	 */
+}
